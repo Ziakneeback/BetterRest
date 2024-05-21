50d6d233b84fc09d904e0279417b052fac8607a7
Zhanibek Asylbek <jonimoni84@gmail.com>
2024-05-21 13:51:21 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Жанибек Асылбек on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
